0b31aa6f7cf1f411d8900f1180d570c23cb5bc94
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-09-25 03:52:06 -0400

    Relic items

---
+++ Items/Tiles/Relics/ConjurerRelicItem.cs
@@ -0,0 +1,30 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Regressus.Tiles.Relics;
+
+namespace Regressus.Items.Tiles.Relics
+{
+    public class ConjurerRelicItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Conjurer Relic");
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+            Item.maxStack = 999;
+            Item.rare = 0;
+            Item.useTurn = true;
+            Item.rare = 0;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.rare = ItemRarityID.Master;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.createTile = ModContent.TileType<ConjurerRelic>();
+        }
+    }
+}
